c6330a39c317871ca75f53b873aa0c0b0fd3a3ce
octa-quintero <octa.quinteroo@gmail.com>
2024-09-13 18:04:27 -0300

    New Logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
+// import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(new LoggerMiddleware().use);
   app.useGlobalPipes(new ValidationPipe({
     whitelist : true //esto es para q cuando haga las validaciones tambien chekee q no te lleguen mas props de las q estan declaradas, con esto nest toma el body q le llega pero lo ignora
   }))
