d199f86ed876900c80406200345b5bba2bdb7e9a
octa-quintero <octa.quinteroo@gmail.com>
2024-09-13 19:19:12 -0300

    Logger:Update

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(new LoggerMiddleware().use);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist : true //esto es para q cuando haga las validaciones tambien chekee q no te lleguen mas props de las q estan declaradas, con esto nest toma el body q le llega pero lo ignora
+    whitelist : true //esto es para q cuando haga las validaciones tambien chekee q no te lleguen mas props de las q estan declaradas, con esto nest toma el body q le llega pero lo ignorar
   }))
-  await app.listen(3000); //cambiar numero de puerto xq el front corren en el mismo
+  await app.listen(3000); //cambiar numero de puerto xq el front corren en el mismo.
 }
 bootstrap();
