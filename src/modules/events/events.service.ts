ae6db495476202682101468959d157c7deeeeca2
Ulises <uliisesrodriguez809@gmail.com>
2024-09-20 12:38:00 -0300

    añadiendo range para amount AND comprobracion de lcoation

--- src/modules/events/events.service.ts
+++ src/modules/events/events.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import {PrismaService} from '../../prisma.service';
+import { PrismaService } from '../../prisma.service';
 import { CreateEventDto } from 'src/modules/events/dto/create-event.dto';
 import { UpdateEventDto } from 'src/modules/events/dto/update-event.dto';
 import { Event } from './entities/events.entity';
@@ -7,33 +7,37 @@ import { Event } from './entities/events.entity';
 @Injectable()
 export class EventsService {
 
-    constructor(private prisma : PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-    // Implementar filtro por QueryParams en el Controller recibiendo los siguientes parametros para el filtrado de los eventos:
-    // type, startDate, endDate
-    // Implementar el service que reciba estos query-params para realizar el filtrado de los eventos a retornar todos los eventos segun los datos del filtro.
-//     Los datos a retornar de cada evento son los siguientes:
-// id, name, type, description, date, time, amount, location
-    getEvents() {
+  // Implementar filtro por QueryParams en el Controller recibiendo los siguientes parametros para el filtrado de los eventos:
+  // type, startDate, endDate
+  // Implementar el service que reciba estos query-params para realizar el filtrado de los eventos a retornar todos los eventos segun los datos del filtro.
+  //     Los datos a retornar de cada evento son los siguientes:
+  // id, name, type, description, date, time, amount, location
+  getEvents() {
 
-      //con los datos de fecha fijate si los eventos estan dentro del rango
+    //con los datos de fecha fijate si los eventos estan dentro del rango
 
-        return this.prisma.event.findMany();
+    return this.prisma.event.findMany();
+  }
+
+  async getEvent(id: number) {
+    try {
+      return await this.prisma.event.findFirst({ where: { id } });
+    } catch (error) {
+      console.error("Error al crear el evento");
+      throw new HttpException('Error al obtener el evento', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
-    async getEvent(id : number){
-        try {
-            return await this.prisma.event.findFirst({where : {id}});
-        } catch (error) {
-            console.error("Error al crear el evento");
-            throw new HttpException('Error al crear el evento',HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  async createEvent(event: CreateEventDto) {
+    try {
+      console.log(event);
+      return await this.prisma.event.create({ data: event });
+    } catch (error) {
+      throw new HttpException('Error al crear el evento', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    createEvent(event : CreateEventDto){
-        console.log(event);
-        return this.prisma.event.create({data : event});
-    }
+  }
 
   updateEvent(id: number, event: UpdateEventDto) {
     return this.prisma.event.update({
@@ -41,8 +45,8 @@ export class EventsService {
       data: event, // actualizar datos
     });
   }
-    
-    
+
+
   // actualizacion de los eventos
   updateEventStatus(id: number, updateData: Partial<UpdateEventDto>) {
     return this.prisma.event.update({
