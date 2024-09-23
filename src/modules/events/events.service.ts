3cecfaf09b6d3b4ec71ee7cf8294c51da78c01a0
Ulises <uliisesrodriguez809@gmail.com>
2024-09-23 19:01:02 -0300

    idea de filtrado de eventos y enum para roles

--- src/modules/events/events.service.ts
+++ src/modules/events/events.service.ts
@@ -14,21 +14,38 @@ export class EventsService {
   // Implementar el service que reciba estos query-params para realizar el filtrado de los eventos a retornar todos los eventos segun los datos del filtro.
   //     Los datos a retornar de cada evento son los siguientes:
   // id, name, type, description, date, time, amount, location
-  getEvents() {
+  async getEvents() {
+    try {
+      // const events = await this.prisma.event.findMany();
 
-    //con los datos de fecha fijate si los eventos estan dentro del rango
+      // //primer filtrado --> type, startDate, endDate
 
-    //falta aplicar la formula matematica del doc
-    //hacer el filtrado segun
+      // const aux = ["evento 1", "evento 2"]
 
-    return this.prisma.event.findMany();
+      // //aplicar formula a cada evento
+      // //dentro de un for para validar cada evento filtrado
+      // const result = raiz(("lat del evento 1" - "lat del usuario")2 + ("long de evento 1" - "long del usuario")2) //usa math javascirpt
+
+      // //if q compara si result <= r 
+
+      // const eventosfiltrados = ["evento 2"]
+
+      //con los datos de fecha fijate si los eventos estan dentro del rango
+
+      //falta aplicar la formula matematica del doc
+      //hacer el filtrado segun
+
+      return await this.prisma.event.findMany();
+    } catch (error) {
+      throw new HttpException('Error al crear el evento', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async getEvent(id: number) {
     try {
       return await this.prisma.event.findFirst({ where: { id } });
     } catch (error) {
-    
+
       throw new HttpException('Error al obtener el evento', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
