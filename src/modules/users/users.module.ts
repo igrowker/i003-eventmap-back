30e87a6503fa2d7488a8c614a897f64eeb7e4384
octa-quintero <octa.quinteroo@gmail.com>
2024-09-25 20:08:33 -0300

    update: users.module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,17 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' }, // porque 60s??
-    })
-  ],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
 })
