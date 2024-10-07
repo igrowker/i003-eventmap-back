8965afd8ee9f5f25b1f7a3e6797e0181dbaf5fcf
aramos20 <adrianjoser20@gmail.com>
2024-10-07 00:46:58 -0400

    ci: Fix

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,16 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthLoginDto } from './dto/auth.login.dto';
 import { CreateUserDto } from './dto/auth.register.dto';
 
-@Controller('auth') //localhost:3000/auth
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')  //localhost:3000/auth/register
+  @Post('register')
   async create(@Res({ passthrough: true}) res: Response, @Body() createUserDto: CreateUserDto) {
     return await this.authService.signUp(createUserDto);
   }
 
-  @Post('login')  //localhost:3000/auth/login
+  @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: AuthLoginDto) {
     return await this.authService.signIn(loginDto);
