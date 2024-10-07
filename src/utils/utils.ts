4ddc66f5558b944c78bc3e3ea2bbefc34693dfbf
Ulises <uliisesrodriguez809@gmail.com>
2024-10-07 16:00:16 -0300

    añadiendo el if q faltaba

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -188,7 +188,6 @@ export const deleteImgCloudinary = async (photos: string[]) => {
                 console.log(result, error);
             });
         }
-
         console.log(response);
     }
 
